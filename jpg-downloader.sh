3cc1914a986bdd14cdb9c7063b6c6f1cda1d52ea
Ahmetcan İrdem <ahmetcan.irdem@ozguryazilim.com.tr>
2023-09-08 17:21:29 +0300

    added todos, removed un using functions

--- jpg-downloader.sh
+++ jpg-downloader.sh
@@ -5,6 +5,8 @@ DOWNDIR="`pwd`/assets"
 SITESEARCH=$2
 SITEMAXCONT=$3
 TMPDIR=/tmp/
+
+# Page count rounder
 round() {
   local dividend="$1"
   local divisor="$2"
@@ -14,27 +16,18 @@ round() {
   echo "$result"
 };
 
-nameCoverPictureParseer() {
-    local downloadedFile="$1"
-
-}
-
-
 RESULT=$( [ ! -e $SITESEARCH-page-1.html ] && curl -s https://$MAINURL/search/$SITESEARCH/ | tee $SITESEARCH-page-1.html | grep -i results | cut -d '-' -f2 | awk '{print $1}')
 PAGING=$(round $RESULT $SITEMAXCONT)
 BEGPAG=1
 
 while [ $BEGPAG -lt $PAGING ]
 do
-
-    #MANGADIR=$(grep cover $SITESEARCH-page-$BEGPAG.html | grep -v meta | cut -d '"' -f2 | tee MANGADIR)
-    #MANGANAME=$(grep caption $SITESEARCH-page-$BEGPAG.html | cut -d '>' -f2 | cut -d '<' -f1 | sed 's/-//g' | sed 's/\.//g' | sed 's/ /_/g' | tee MANGANAME)
-    #MANGAPICLOC=$(grep src $SITESEARCH-page-$BEGPAG.html | grep pics | cut -d '"' -f8 | tee MANGAPICLOC)
-    
+    # Parse manga dir name and picture location to files
     grep cover $SITESEARCH-page-$BEGPAG.html | grep -v meta | cut -d '"' -f2 > MANGADIR
     grep caption $SITESEARCH-page-$BEGPAG.html | cut -d '>' -f2 | cut -d '<' -f1 | sed 's/-//g' | sed 's/\.//g' | sed 's/ /_/g' > MANGANAME
     grep src $SITESEARCH-page-$BEGPAG.html | grep pics | cut -d '"' -f8 > MANGAPICLOC
-    
+   
+    # Merge all manga dir name and picture in same line seperated by ";" char 
     MANGAVARS=$(paste -d ';' MANGADIR MANGANAME MANGAPICLOC)
     
     # Enter each manga site for page and content
@@ -44,6 +37,8 @@ do
         MANGADIR=$(echo $MANGA | cut -d ';' -f1)
         MANGANAME=$(echo $MANGA | cut -d ';' -f2)
         MANGAPICDIR=$(echo $MANGA | cut -d ';' -f3)
+        
+        # TODO Check MANGANAME if exists skip
 
         # Create Download dir, set download url and check how much manga page had, download selected manga poster
         mkdir -p $DOWNDIR/$MANGANAME
@@ -60,6 +55,9 @@ do
     done
     
     BEGPAG=$(($BEGPAG + 1))
+    # TODO Each founded page must be download and has to be remove after finished downloading
+    # TODO Each page mus be resulted by BEGPAG variable, every page manga content and manga dirs should be downloaded
+    ## RESULT=$( [ ! -e $SITESEARCH-page-1.html ] && curl -s https://$MAINURL/search/$SITESEARCH/ | tee $SITESEARCH-page-1.html | grep -i results | cut -d '-' -f2 | awk '{print $1}')
     exit 0
 done
 
